e1dd359e5462704c3593b34fea4c988ad84a55be
JagTheFriend <jagthefriend12@gmail.com>
2021-04-19 20:11:50 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from app_class import App
+from Pacman import main
+# use:
+# from Another_Game import main
+# If you want to run the other game
 
 if __name__ == '__main__':
-    app = App()
-    app.run()
+    main.run()
